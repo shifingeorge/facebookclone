9579ef34210c6b186dd0c09d2587f6d13e987eda
shifingeorge <75887575+shifingeorge@users.noreply.github.com>
2022-02-17 23:21:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function cnew()
+{
+    alert("Thanks for clicking :) but its a Demo")
+}
+function dis()
+{
+    var input = document.getElementById("em").value
+    var pas = document.getElementById("pa").value
+    alert(input)
+    alert("*******")
+}
